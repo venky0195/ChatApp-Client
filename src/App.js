11d1a3d374e08ade61b2bd4a7ab6e27190f26b65
venky0195 <ven.venky08@gmail.com>
2019-02-23 19:15:28 +0530

    Completed FrontEnd

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Login from "./screens/login";
+import Register from "../src/screens/register";
+import forgotPassword from "../src/screens/forgotPassword";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import resetPassword from "./screens/resetPassword";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <div className="App">
+            {<Route path="/" component={Login} />}
+            <Route path="/forgotPassword" component={forgotPassword} />
+            <Route path="/register" component={Register}/>
+            <Route path="/resetPassword" component={resetPassword}/>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
